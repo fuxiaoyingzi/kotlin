a7f192560cb333f13aafd9ff62a2c43a28b54248
fuxiaoyingzi <smileying12@126.com>
2018-06-01 19:03:00 +0800

    kotlin 电商项目首次提交

---
+++ App/src/main/java/kotlin/shadow/com/kotlinshop/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package kotlin.shadow.com.kotlinshop.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.shadow.com.kotlinshop.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
