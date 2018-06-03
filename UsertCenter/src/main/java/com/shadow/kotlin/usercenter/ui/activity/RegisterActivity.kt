56e2c4440cbf371fce74a693527086df8ed89eb3
fuxiaoyingzi <smileying12@126.com>
2018-06-03 10:19:00 +0800

    动态切换application和library

---
+++ UsertCenter/src/main/java/com/shadow/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.shadow.kotlin.usercenter.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.shadow.kotlin.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
